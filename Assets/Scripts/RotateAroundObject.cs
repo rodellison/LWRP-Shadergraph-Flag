c40da9d616b0e9b5569261226d3a2689b23f063c
Rod Ellison <rod.r.ellison@gmail.com>
2019-05-11 14:49:54 -0400

    first commit

---
+++ Assets/Scripts/RotateAroundObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour
+{
+    public float speed;
+    public bool rotateAroundCenter = false;
+    public GameObject objectToRotateAround;
+    public GameObject objectToLookAt;
+    public bool lookAtObject = false;
+    private bool objectToLookAtNotNull = false;
+    public bool OscillateVertical = false;
+    public float OscillateAmount;
+    private float minY = 0;
+    private float maxY = 0;
+
+
+    private Vector3 rotateCenter;
+    
+    // Update is called once per frame
+    void Start()
+    {
+        rotateCenter = new Vector3(0,0,0);
+        if (!rotateAroundCenter)
+        {
+            if (objectToRotateAround != null)
+            {
+                float currentY = transform.localPosition.y;
+                Vector3 objectToRotateAroundPos = objectToRotateAround.transform.localPosition;
+                //keep the y (height) to be that of this objects transform, but set the center x/z to that of the objectToRotateAround
+                transform.localPosition = new Vector3(objectToRotateAroundPos.x, currentY, objectToRotateAroundPos.z);
+            }
+       }
+        if (lookAtObject && objectToLookAt != null)
+            objectToLookAtNotNull = true;
+
+        if (OscillateVertical)
+        {
+            minY = transform.localPosition.y - OscillateAmount;
+            maxY = transform.localPosition.y + OscillateAmount;
+        }
+        
+    }
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+        if (objectToLookAtNotNull)
+            Camera.main.transform.LookAt(objectToLookAt.transform);
+            transform.position = new Vector3(transform.position.x, Mathf.SmoothStep(minY, maxY, Mathf.PingPong(Time.time * 0.1f ,1)),  transform.position.z);
+  
+        
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+### **LWRP-Shadergraph-Flag**
+
+Simple Unity project, using Shadergraph to create a vertex animated flag shader
+ 
+![Project View](./Capture.JPG)
